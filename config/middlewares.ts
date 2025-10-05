0bc0eb875df36a67576329619c27f21bf2648d87
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-10-05 11:36:32 +0600

    Update CORS and session config for frontend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,33 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        env('FRONTEND_URL'),   // uses env variable
+        'http://localhost:3000', // dev local
+      ],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true, // allow cookies
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      secretKeys: env.array('APP_KEYS'),
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+      },
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
